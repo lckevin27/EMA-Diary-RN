28c156df45b9f2aed6f165ac9c6a02209f8bab2e
AdosH1 <aden.huen@gmail.com>
2021-03-24 09:35:02 +0800

    Fix missing ios loading modal

--- SurveyScreen.ios.js
+++ SurveyScreen.ios.js
@@ -31,7 +31,6 @@ class SurveyScreen extends React.Component {
       let serverData = navigation.getParam('serverData', 'Unable to find server data.');
 
       if (!SurveyScreenShared.CheckServerData(this, serverData)) {
-        console.log("I AM RETURNING WITHOUT DOING ANYTHING ELSE")
         return;
       }
 
@@ -158,6 +157,18 @@ class SurveyScreen extends React.Component {
                     </View>: null
                   }
 
+                  <View>
+                    <Modal isVisible={this.state.showLoadingModal}>
+                      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={{backgroundColor: '#FFF', width: 330, height: 110, justifyContent: 'center', alignItems: 'center', padding: 10 }}>
+                          {!this.state.showModalOkayButton ? <ActivityIndicator size="large" color="#00ff00" /> : null }
+                          <Text style={{fontSize: 16}}>{this.state.modalText}</Text>
+                          {this.state.showModalOkayButton ? <Button title="Ok" buttonStyle={{marginTop: 15, width: this.state.screenWidth / 3, height: this.state.screenHeight / 20}} onPress={() => {SurveyScreenShared.closeModal(this)}}></Button> : null}
+                        </View>
+                      </View>
+                    </Modal>
+                  </View> 
+
                     {this.state.ViewArray.map(info => info)}
                   </View>
                   <View style={{ height: 50, }}></View>
@@ -229,7 +240,7 @@ const styles = StyleSheet.create({
       //marginBottom: 35
   },
   colorYellow: {
-      color: 'rgb(252, 228, 149)',
+      color: 'yellow',
       transform: [{ rotate: '-90deg'}]
   }
 });
